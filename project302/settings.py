f84ac44b7fac756088983ef7b6532c7c06a7308a
adamts028 <adamts028@gmail.com>
2023-06-30 03:16:11 +1200

    Deployment Test

--- project302/settings.py
+++ project302/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
-    'project302',
+    'kiwinco',
     'rest_framework',
     'rest_framework_simplejwt',
     
